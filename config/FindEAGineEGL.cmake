db5be29e8a8a2efa4786925ca593fe1cc0668330
Matus Chochlik <chochlik@gmail.com>
2021-06-20 09:07:02 +0200

    Changes in package detection in the build system + updated sub-modules

--- config/FindEAGineEGL.cmake
+++ config/FindEAGineEGL.cmake
@@ -3,5 +3,9 @@
 #  See accompanying file LICENSE_1_0.txt or copy at
 #   http://www.boost.org/LICENSE_1_0.txt
 #
-eagine_common_import_lib(EGL egl EGL/egl.h EGL)
-
+eagine_common_import_lib(
+	PREFIX EGL
+	PKGCONFIG egl
+	HEADER EGL/egl.h
+	LIBRARY EGL
+)
