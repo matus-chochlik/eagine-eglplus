c62e2bd80fc11ace424599696d3d85f3c1b539cc
Matus Chochlik <chochlik@gmail.com>
2023-02-19 05:12:31 +0100

    Added tests for constant conversions

---
+++ source/modules/eagine/constants_test.cpp
@@ -0,0 +1,39 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+#include <eagine/testing/unit_begin.hpp>
+import eagine.core;
+import eagine.eglplus;
+import <set>;
+//------------------------------------------------------------------------------
+void constants_enum_by_name(auto& s) {
+    eagitest::case_ test{s, 1, "EGL enum by name"};
+    std::set<eagine::eglplus::egl_types::enum_type> distinct;
+
+    const auto check{[&](eagine::string_view name) {
+        if(auto value{eagine::eglplus::egl_enum_by_name(name)}) {
+            distinct.insert(extract(value));
+        } else {
+            test.fail(name);
+        }
+    }};
+    check("luminance_buffer");
+    check("no_texture");
+    check("rgb_buffer");
+    check("texture_2d");
+
+    test.check_equal(distinct.size(), 4U, "constant count");
+}
+//------------------------------------------------------------------------------
+auto main(int argc, const char** argv) -> int {
+    eagitest::suite test{argc, argv, "constants", 1};
+    test.once(constants_enum_by_name);
+    return test.exit_code();
+}
+//------------------------------------------------------------------------------
+#include <eagine/testing/unit_end.hpp>
