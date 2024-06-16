2e9bf4d0d110a3999d75bc3e14c65a849069c57c
Matus Chochlik <chochlik@gmail.com>
2024-06-16 11:37:08 +0200

    Added the examples install component + updated sub-modules

--- config/EAGinePackageEGLplus.cmake
+++ config/EAGinePackageEGLplus.cmake
@@ -6,9 +6,12 @@
 # Package specific options
 #  Debian
 #   Dependencies
-set(CPACK_DEBIAN_EGLPLUS-APPS_PACKAGE_DEPENDS "")
+set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
+set(CPACK_DEBIAN_EGLPLUS-APPS_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
+set(CPACK_DEBIAN_EGLPLUS-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
 set(CPACK_DEBIAN_EGLPLUS-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@)")
 #   Descriptions
 set(CPACK_DEBIAN_EGLPLUS-APPS_DESCRIPTION "Collection of EGLplus applications.")
+set(CPACK_DEBIAN_EGLPLUS-EXAMPLES_DESCRIPTION "EAGine EGLplus examples.")
 set(CPACK_DEBIAN_EGLPLUS-DEV_DESCRIPTION "C++ wrapper for EGL.")
 
